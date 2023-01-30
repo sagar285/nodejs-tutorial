d5e97d6f2f2f8bfbfba31bf2418bbd48be7a537f
sagar <anandguptasir@gmail.com>
2023-01-30 14:11:52 +0530

    change comit

---
+++ path modules/index.js
@@ -0,0 +1,8 @@
+const path = require("path");
+ const a=path.basename("/foo/bar/baz/asdf/quux.html")
+ const b= path .dirname("/foo/bar/baz/asdf/quux.html")
+ const c= path .extname("/foo/bar/baz/asdf/quux.html")
+ console.log(a);
+ console.log(b);
+ console.log(c);
+ 
