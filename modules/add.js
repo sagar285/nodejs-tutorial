80453806b142d8e912eae171bf2a0382f90e595d
sagar <anandguptasir@gmail.com>
2023-01-30 14:03:28 +0530

    initial comit

---
+++ modules/add.js
@@ -0,0 +1,5 @@
+function add(a,b){
+    return a+b;
+}
+const sum = add(2,3);
+console.log(sum);
---
+++ modules/index.js
@@ -0,0 +1,7 @@
+let a="sagar"
+console.log(a);
+ const add=require("./sub")
+ const sum=add(2,1)
+ console.log(sum);
+
+
