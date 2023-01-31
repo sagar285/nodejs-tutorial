66664868b9938002365fd772dd70f9cef2fc0e02
sagar <anandguptasir@gmail.com>
2023-01-31 08:45:36 +0530

    change

--- express/index.js
+++ express/index.js
@@ -1,23 +1,31 @@
-const express =require("express");
+const express = require("express");
 const path = require("path");
-const app = express()
+const app = express();
+
+// const pathdir = path.join(__dirname, "public");
+// app.use(express.static(pathdir));
+
+app.set("view engine", "ejs");
 
 
- const pathdir=path.join(__dirname,'public')
- app.use(express.static(pathdir))
+app.get("/", (req, res) => {
+  // res.sendFile(`${pathdir}/inde.html`);
+  res.render("inde",{
+    title:"index"
+  })
+});
 
+app.get("/about", (req, res) => {
+  // res.sendFile(`${pathdir}/home.html`);
+  res.render("home",{
+    title:"home"
+  })
+});
 
+app.get("/download", (req, res) => {
+  // res.download(`${pathdir}/inde.html`);
+});
 
-app.get("/",(req,res)=>{
- res.sendFile(`${pathdir}/inde.html`);
-})
-app.get("/about",(req,res)=>{
-    res.sendFile(`${pathdir}/home.html`);
-   })
-   app.get("/download",(req,res)=>{
-  res.download(`${pathdir}/inde.html`)
-   })
-
-app.listen(3000,()=>{
-    console.log("server established");
-})
+app.listen(3000, () => {
+  console.log("server established");
+});
